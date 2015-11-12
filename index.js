a7544df5bf674d5feba9ebf2ec60179249f7f55f
Mark Stier <stier020@Marks-MacBook-Pro.local>
2015-11-12 12:07:30 -0600

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict"
+
+var Worker = require("basic-distributed-computation").Worker;
+var Stream = require("stream").Readable;
+
+class PipeToBuffer extends Worker {
+  constructor(parent){
+    super("pipe-to-buffer", parent);
+  }
+
+  work(req, inputKey, outputKey){
+    var inVal = req.body;
+    if(inputKey){
+      inVal = req.body[inputKey];
+    }
+    
+    toArray(inVal)
+      .then(function (parts) {
+        var buffers = []
+        for (var i = 0, l = parts.length; i < l ; ++i) {
+          var part = parts[i]
+          buffers.push((part instanceof Buffer) ? part : new Buffer(part))
+        }
+        return Buffer.concat(buffers)
+      }).then((buffer) => {
+        if(outputKey){
+          req.body[outputKey] = buffer;
+        } else {
+          req.body = buffer;
+        }
+      }).catch((err) => {
+        req.status(err);
+      }).finally(() => {
+        req.next();
+      })
+
+    }
+  }
+}
+
+module.exports = PipeToBuffer;
